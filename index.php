3cbefead118226ae22c20127c9b54e8197b4cb74
Wloyer <113607334+Wloyer@users.noreply.github.com>
2023-10-11 14:48:04 +0200

    php

--- index.php
+++ index.php
@@ -11,84 +11,15 @@
 </head>
 
 <body style="height:100vh">
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom pe-5 ps-5">
-        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-            <span class="w-50 text-white fs-2">AlphaWave</span>
-        </a>
-
-        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="/" class="nav-link px-2 active link-light">Accueil</a></li>
-            <li><a href="/views/produit.html" class="nav-link px-2 link-light">Produits</a></li>
-            <li><a href="#" class="nav-link px-2 link-light">Contactez Nous</a></li>
-            <li><a href="#" class="nav-link px-2 link-light">A propos</a></li>
-        </ul>
-
-        <div class="col-md-3 text-end">
-            <button type="button" class="btn btn-outline-primary me-2 text-white" style="border: none !important">Connexion</button>
-            <button type="button" class="btn btn-primary" style="border: none !important">Inscription</button>
-        </div>
-    </header>
-      <img id="middleImage" src="./public/images/middleImg.jpg" alt="city">
-
-    <div class="cardCustom container">
-      <div class="card" style="width: 18rem;">
-        <img src="./public/images/LogoForward.jpg" class="card-img-top imgCardCustom" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-      <div class="card" style="width: 18rem;">
-        <img src="./public/images/LogoLeft.jpg" class="card-img-top imgCardCustom" alt="...">
-        
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-      <div class="card" style="width: 18rem;">
-        <img src="./public/images/LogoPath.jpg" class="card-img-top imgCardCustom" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-      <div class="card" style="width: 18rem;">
-        <img src="./public/images/LogoRight.jpg" class="card-img-top imgCardCustom" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
+    
+<?php   require_once 'views/includes/header.php';?>
+    
+<?php   require_once 'views/acceuil.php';?>
 
 
+<?php   require_once 'views/includes/footer.php';?>
 
-    <div class="cookies">
-        <div class="cookie-texte">
-            <p class="text-white">En cochant cette case, je reconnais avoir lu, compris et accepté les Conditions Générales d'Utilisation du service</p>
-        </div>
-        <div class="cookies-button">
-            <button class="btn btn-success">Accepter</button>
-            <button class="btn btn-deny">Parameters</button>
-        </div>
-    </div>
-    <div class=" footerCustom">
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <p class="col-md-4 mb-0 text-body-secondary">&copy; 2023 Company, Inc</p>
-      
-          <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-          </a>
-      
-          <ul class="nav col-md-4 justify-content-end">
-            <li class="nav-item"><a href="#" class="PoliceFooter">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
-          </ul>
-        </footer>
+
         
       </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
--- views/includes/header.php
+++ views/includes/header.php
@@ -1,4 +1,4 @@
-<div class="container">
+
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
       <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
         <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
@@ -17,4 +17,4 @@
         <button type="button" class="btn btn-primary">Sign-up</button>
       </div>
     </header>
-  </div>
+
